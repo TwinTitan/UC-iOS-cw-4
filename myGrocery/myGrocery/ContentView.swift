f5537992613bc0352816c7f1ec890bd4dba06aa1
TwinTitan <abdulla.al.shammeri@gmail.com>
2022-09-12 20:30:20 +0300

    cw4

---
+++ myGrocery/myGrocery/ContentView.swift
@@ -0,0 +1,78 @@
+//
+//  ContentView.swift
+//  myGrocery
+//
+//  Created by Abdallah Alshemeri on 12/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var name = ""
+    @State var Items = ["RedBull", "Juice", "Pepsi", "Mars", "Chips"]
+    var body: some View {
+        VStack{
+            List(Items, id: \.self){ Item in
+                HStack{
+                    Image(Item)
+                        .resizable()
+                        .frame(width:15, height: 30)
+                    Text(Item)
+                        .font(.title2)
+                }
+            }
+                HStack{
+                    
+                    Button(action: {
+                        if !name.isEmpty{
+                        Items.append(name)
+                        }
+                
+                    }) {
+                        Image(systemName: "plus")
+                            .foregroundColor(Color.white)
+                            .frame(width: 40, height: 40)
+                            .background(.blue)
+                            .clipShape(Circle())
+                            .padding(.leading)
+
+                    }
+                    TextField("Item Name", text: $name)
+                    
+                    Button(action: {
+                        if !Items.isEmpty{
+                            Items.remove(at:0)
+                        }
+                        
+                
+                    }) {
+                        Image(systemName: "minus")
+                            .foregroundColor(Color.white)
+                            .frame(width: 40, height: 40)
+                            .background(.red)
+                            .clipShape(Circle())
+                            .padding(.leading)
+                    }
+                    Button(action: {
+                        Items.append(Items.randomElement() ?? "")
+                
+                    }) {
+                        Image(systemName: "questionmark")
+                            .foregroundColor(Color.white)
+                            .frame(width: 40, height: 40)
+                            .background(.yellow)
+                            .clipShape(Circle())
+                            .padding(.trailing)
+                    }
+                    
+            }
+        }
+       
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ myGrocery/myGrocery/myGroceryApp.swift
@@ -0,0 +1,17 @@
+//
+//  myGroceryApp.swift
+//  myGrocery
+//
+//  Created by Abdallah Alshemeri on 12/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct myGroceryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
